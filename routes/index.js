a36d97b6df5334587bcd92935d63daf114f175bf
cediazv <cediazv@unbosque.edu.co>
2017-05-06 20:39:08 -0500

    Versión inicial
    Primera versión del sitio web del proyecto

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var api = require('../utilities/api');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', other: 'Bitch', palabra: 'calvario' });
+});
+
+router.get('/saludo', function(req, res, next) {
+  api.get('/test', function(data){
+    res.render('saludo', { title: 'bitch', uri: 'https://ccafeueb-s.herokuapp.com/test', objeto: JSON.stringify(data) });
+  },
+  function(er){
+    res.render('index', { title: 'Express', other: 'Bitch', palabra: 'calvario' });
+  });
+});
+
+module.exports = router;
---
+++ utilities/api.js
@@ -0,0 +1,30 @@
+var https = require('https');
+
+var options = {
+  host: 'ccafeueb-s.herokuapp.com',
+  port: 80,
+  path: '',
+  method: ''
+}
+
+var get = function(uri, response, error){
+  options.path = uri;
+  options.method = 'GET';
+
+  var req = https.request(options, function(res){
+    res.setEncoding('utf8');
+    res.on('data', function(chunk){
+      response(JSON.parse(chunk));
+    });
+  });
+
+  req.on('error', error);
+  //req.write(ALGO);
+  req.end();
+}
+
+
+// Export the functions for external access
+module.exports = {
+  get: get
+};
