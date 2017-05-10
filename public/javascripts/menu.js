d5d19c9b59587824c2f814d3cfa337e056efdc7e
cediazv <cediazv@unbosque.edu.co>
2017-05-09 23:40:06 -0500

    Prueba
    Prueba

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -1,5 +1,10 @@
 (function(){
 	var load = function(){
+		manageMenu();
+		cargarMenu();
+	}
+
+	var manageMenu = function(){
 		var btnContainer = document.querySelector('.menu-container');
 		var btn = document.querySelector('.btn-menu > button');
 
@@ -11,6 +16,25 @@
 				btnContainer.classList.add('show');
 			}
 		}
+	}
+
+	var cargarMenu = function(){
+		//https://ccafeueb-s.herokuapp.com/committees
+        var http = new XMLHttpRequest();
+        http.open('GET', 'https://ccafeueb-s.herokuapp.com/committees', true);
+        http.setRequestHeader('Accept', 'application/json, text/javascript');
+        http.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+        http.onreadystatechange = function () {
+            if (http.readyState == 4) {
+                if (http.status == 200) {
+                	debugger
+                }
+            }
+        };
+        http.send();
+
+        return http;
 	}
 
 	window.onload = function(){
